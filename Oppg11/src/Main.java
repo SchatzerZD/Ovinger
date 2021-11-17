166450a0561c1337e301610b8655a2d0ce9f5795
SchatzerZD <daniel.walaker@gmail.com>
2021-11-17 14:47:25 +0100

    final oppg11

--- Oppg11/src/Main.java
+++ Oppg11/src/Main.java
@@ -1,5 +1,9 @@
+
 class Main {
     public static void main(String[] args) {
+
+
+
         Menu menu = new Menu();
         menu.start();
     }
--- Oppg11/src/Menu.java
+++ Oppg11/src/Menu.java
@@ -66,6 +66,13 @@ public class Menu {
     public void start() {
         boolean finished = false;
         PropertyRegistry propertyRegistry = new PropertyRegistry();
+
+        propertyRegistry.registerProperty(1445, "Gloppen",77,631,"",1017.6,"Jens Olsen");
+        propertyRegistry.registerProperty(1445, "Gloppen",77,131,"Syningom",661.3,"Nicolay Madsen");
+        propertyRegistry.registerProperty(1445,"Gloppen",75,19,"Fugletun",650.6,"Evilyn Jensen");
+        propertyRegistry.registerProperty(1445,"Gloppen",74,188,"",1457.2,"Karl Ove Bråten");
+        propertyRegistry.registerProperty(1445,"Gloppen",69,47,"Høiberg",1339.4, "Elsa Indregård");
+
 // The while-loop will run as long as the user has not selected
 // to quit the application
         while (!finished) {
